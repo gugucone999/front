a0bd3925a11b661f539ad6b73d81dc92438a3f7b
gugucone999 <gugucone999@gmail.com>
2023-04-28 16:13:16 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [datas, setDatas] = React.useState([]);
+  React.useEffect(() => {
+    fetch("http://192.168.181.10:32563/dev/data", {
+      method: "GET",
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        setDatas(res.result);
+      });
+  }, []);
+
+
+
+
+  return (
+    <div className="App">
+      {datas.map((data) => {
+        return <div key={data.id}>{data.value}</div>
+      })}
+    </div>
+  );
+}
+
+export default App;
